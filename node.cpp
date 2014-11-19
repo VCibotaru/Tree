e8201271f4f7398b3818b5bf475a659da37993fb
Viktor Chibotaru <vcibotaru@gmail.com>
2014-11-20 00:28:34 +0400

    Revert "Revert b58f4a5..c5dd9cf"
    This reverts commit 6bfb3e8aa1097404b2de9b69875c1c94191db764.

--- node.cpp
+++ node.cpp
@@ -5,21 +5,22 @@
 
 #define MAX_SLOTS 9
 
-Node::Node(Tree *tr, int _s, int _m_s, bool Leaf, float _x, float _y, float _z, float s_x, float s_y, float s_z, float p, float t, Node *parent):
+Node::Node(Tree *tr, int _s, int _m_s, bool Leaf, 
+	float s_x, float s_y, float s_z, 
+	float p, float t, float h, Node *_parent):
 tree(tr),
 slotBusy(MAX_SLOTS, false),
 step(0),
 slot(_s),
 max_step(_m_s),
 isLeaf(Leaf),
-x(_x),
-y(_y),
-z(_z),
 scale_x(s_x),
 scale_y(s_y),
 scale_z(s_z),
 phi(p),
-theta(t)
+theta(t),
+height(h),
+parent(_parent)
 {
 	if (Leaf) {
 		tree->incLeaves();
@@ -27,6 +28,17 @@ theta(t)
 	else {
 		tree->incBranches();
 	}
+	modelMatrix = glm::mat4();
+	if (!_parent) {
+		modelMatrix = glm::translate(modelMatrix, glm::vec3(ROOT_TRANS_X, ROOT_TRANS_Y, ROOT_TRANS_Z));
+	}
+	else {
+		glm::vec4 tr = _parent->getModelMatrix() * glm::vec4(0.0f, height, 0.0f, 1.0f);
+		modelMatrix = glm::translate(modelMatrix, glm::vec3(tr.x, tr.y, tr.z));
+	}
+	modelMatrix = glm::rotate(modelMatrix, theta, glm::vec3(0.0f, 1.0f, 0.0f));
+	modelMatrix = glm::rotate(modelMatrix, phi, glm::vec3(0.0f, 0.0f, 1.0f));
+	modelMatrix = glm::scale(modelMatrix,glm::vec3(scale_x, scale_y, scale_z));
 }
 
 void Node::grow() {
@@ -38,6 +50,7 @@ void Node::grow() {
 		scale_x += STEP_X; 
 		scale_y += STEP_Y;
 		scale_z += STEP_Z;
+		modelMatrix = glm::scale(modelMatrix,glm::vec3(scale_x, scale_y, scale_z));
 	}
 	for (unsigned i = 0 ; i < children.size() ; ++i) {
 		children[i].grow();
@@ -61,15 +74,14 @@ void Node::addChildren() {
 	float newPhi = getRandomPhi();
 	float newTheta = getRandomTheta();
 	float t = curSlot * 1.0f / (MAX_SLOTS + 1) + 0.1f;
-	glm::vec4 end = getBranchEnd(t);
 	if (tree->canAddBranch()) {
-		Node child(tree, curSlot, max_step, false, end.x, end.y, end.z, scale_x / 2, scale_y / 2, scale_z / 2, newPhi, newTheta, this);
+		Node child(tree, curSlot, max_step, false, scale_x / 2, scale_y / 2, scale_z / 2, newPhi, newTheta, t, this);
 		children.push_back(child);
 		slotBusy[curSlot] = true;
 	}
 	else {
 		const float LEAF_SCALE = 0.02f;
-		Node child(tree, curSlot, max_step, true, end.x, end.y, end.z, LEAF_SCALE, LEAF_SCALE, LEAF_SCALE, newPhi, newTheta, this);
+		Node child(tree, curSlot, max_step, true, LEAF_SCALE, LEAF_SCALE, LEAF_SCALE, newPhi, newTheta, t, this);
 		children.push_back(child);
 		slotBusy[curSlot] = true;
 	}
@@ -109,13 +121,3 @@ glm::vec4 Node::getBranchEnd(float t) {
 	return getModelMatrix() * glm::vec4(0.0f, t, 0.0f, 1.0f);
 }
 
-glm::mat4x4 Node::getModelMatrix() {
-	glm::mat4x4 modelMatrix = glm::mat4();
-
-	modelMatrix = glm::translate(modelMatrix,glm::vec3(x, y, z));
-	modelMatrix = glm::rotate(modelMatrix, theta, glm::vec3(0.0f, 1.0f, 0.0f));
-	modelMatrix = glm::rotate(modelMatrix, phi, glm::vec3(0.0f, 0.0f, 1.0f));
-	modelMatrix = glm::scale(modelMatrix,glm::vec3(scale_x, scale_y, scale_z));
-
-	return modelMatrix;
-}
